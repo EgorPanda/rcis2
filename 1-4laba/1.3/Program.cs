7cf87b1e9e44caabaeb3f318a50bbca48f39021c
EgorPanda <119073728+EgorPanda@users.noreply.github.com>
2023-03-04 10:24:34 +0700

    labi

---
+++ 1-4laba/1.3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            string str;
+            int[] nums = { 1,  4, 3, 2,1 };
+            bool a = false;
+
+            for (int j = 0; j < nums.Length-1; j++)
+            {
+                for (int i = 1+j; i < nums.Length; i++)
+                {
+                    if (nums[j] == nums[i])
+                    {
+                        a = true;
+                    }
+                }
+            }
+
+            if (a == false)
+            {
+                str = "False";
+            }
+            else
+            {
+                str = "True";
+            }
+
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ 1-4laba/3.1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Worker
+    {
+        public string name, surname;
+        public int days, rate;
+
+        public void GetSalary()
+        {
+            Console.WriteLine($"ЗАРПЛАТА -  {days * rate}");
+        }
+    }
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Worker sanya = new Worker();
+            sanya.name = "shuric";
+            sanya.surname = "lukianov";
+            sanya.days = 5;
+            sanya.rate = 40;
+            sanya.GetSalary();
+        }
+    }
+}
